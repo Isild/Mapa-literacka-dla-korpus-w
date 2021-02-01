ac0a5391ab075a6f45ac857056891e3d1f305c49
Isild <pimok96@gmail.com>
2021-02-01 10:51:31 +0100

    clear prints and comments

--- serwer/app/controllers/text_analyze.py
+++ serwer/app/controllers/text_analyze.py
@@ -21,12 +21,6 @@ class TextAnalyze(MethodView):
     def get(self):
         lm_id = request.args.get('id')
         lm = LiteraryMap.query.filter_by(id=lm_id).first()
-        # obszar testowy
-        # x1 = -102
-        # y1 = 52
-        # x2 = 54
-        # y2 = 30
-        ################
         lmJson = lm.toJSON()
         if request.args.get('x1') and request.args.get('y1') and request.args.get('x2') and request.args.get('y2'):
             x1 = float(request.args.get('x1'))
@@ -38,11 +32,6 @@ class TextAnalyze(MethodView):
             d = x2 - x1
             h = y1 - y2
             maxPointAmount = 10
-
-            # print(lm.toJSON()['nodesData'])
-            print("\n")
-            print("\n")
-
             
             locations = lm.toJSON()['nodesData']
             maxTime = 0
@@ -51,8 +40,6 @@ class TextAnalyze(MethodView):
                 if (maxTime < int(loc['time'])):
                     maxTime = int(loc['time'])
 
-            # myMap = [[20], [10]]
-            # myMap = [[[]]*20]*10
             myMap = []
             for i in range(0, 20):
                 row = []
@@ -60,29 +47,19 @@ class TextAnalyze(MethodView):
                     row.append([])
                 myMap.append(row)
 
-            print(myMap)
-            print("\n")
-            print(locations)
+            print("dupa")
+            
             p = 0
             for loc in locations:
-                print(loc)
                 xP = loc['coords']['lng']
                 yP =loc['coords']['lat']
-                # if p < 3:
-                # print("? ", x2, " ", xP, " ",
-                #       x1, " ; ", y1, " ", yP, " ", y2)
-                # print(x2 >= xP,
-                #       " ", xP >= x1, " ", y1 >= yP, " ", yP >= y2)
+
                 if x2 >= xP and xP >= x1 and y1 >= yP and yP >= y2:
                     xMyMap =getColumnNumber((
                         loc['coords']['lng']), x1, wW, d)
                     yMyMap =getRowNumber((loc['coords']['lat']), y2, wH, h)
                     myMap[xMyMap][yMyMap].append(loc)
                     p = p + 1
-                    # print("## ", xMyMap, " ", yMyMap, " ", loc['name'])
-                    # print(loc['name'], " : ", loc['coords'], " ", loc['coords']
-                        #   ['lat'], " ", loc['coords']['lng'], ", time in book: ", int(int(loc['time'])/maxTime*100))
-            print("max time: ", maxTime)
 
             newPointsList =  []
             for row in myMap:
@@ -90,17 +67,12 @@ class TextAnalyze(MethodView):
                     # print(places, ", len: ", len(places))
                     it = 0
                     for p in places:
-                        if it < maxPointAmount: 
-                            newPointsList.append(p)
-                            print("_",p)
-                            it = it+1
-                # print("#\n")
+                        newPointsList.append(p)
+                        # if it < maxPointAmount: 
+                        #     newPointsList.append(p)
+                        #     print("_",p)
+                        #     it = it+1
 
-            print(newPointsList, "\n")
-
-            # print(myMap[0][0][1]) #tutaj jeszcze znać size, żeby sprawdzić ile gdzie jest punktów
-            # print("\n", myMap)
-            print(lm.toJSON(), "\n")
             newReturnList = {}
             newReturnList["id"] = lmJson['id']
             newReturnList["nodesData"] = newPointsList
@@ -109,15 +81,13 @@ class TextAnalyze(MethodView):
             newReturnList["status"] = lmJson['status']
             newReturnList["maxTime"] = maxTime
 
-            print(newReturnList)
-
             if lm is not None:
                 return lm.toJSON()
             else:
                 return abort(404)
         elif request.args.get('timeStart') and request.args.get('timeEnd'):
-            timeStart = 20#request.args.get('timeStart')
-            timeEnd = 80#request.args.get('timeEnd')
+            timeStart = int(request.args.get('timeStart'))
+            timeEnd = int(request.args.get('timeEnd'))
             locations = lm.toJSON()['nodesData']
             maxTime = 0
             for loc in locations:
@@ -125,16 +95,12 @@ class TextAnalyze(MethodView):
                 if (maxTime <= (loc['time'])):
                     maxTime = (loc['time'])
 
-            print("max time: ", maxTime)
             pointsFromTimeWindow = []
             for loc in locations:
                 loc['time'] = (loc['time']/maxTime)*100
-                # print((loc['time']/maxTime)*100)
-                # print("loc: ", loc)
                 if loc['time'] >= timeStart and timeEnd >= loc['time']:
                     pointsFromTimeWindow.append(loc)
 
-            print(pointsFromTimeWindow)
             newReturnList = {}
             newReturnList["id"] = lmJson['id']
             newReturnList["nodesData"] = pointsFromTimeWindow
@@ -145,7 +111,6 @@ class TextAnalyze(MethodView):
             newReturnList["timeStart"] = timeStart
             newReturnList["timeEnd"] = timeEnd
 
-            print(newReturnList)
             if lm is not None:
                 return newReturnList
             else:
